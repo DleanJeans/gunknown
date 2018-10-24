ff665b627f6ba73cff5a209b62df38c491c7bb7e
Dlean Jeans <dleanjeans@gmail.com>
2018-10-24 16:59:23 +0700

    Fix BulletTrail render artifact

--- godot/source/feel/BulletTrail.gd
+++ godot/source/feel/BulletTrail.gd
@@ -5,11 +5,13 @@ onready var bullet = get_parent()
 onready var bullet_ref = weakref(bullet)
 
 func _ready():
+	add_point(bullet.position)
+	add_point(bullet.position)
+	
 	bullet.connect('tree_exiting', $AnimationPlayer, 'play', ['Fade'])
 	yield($AnimationPlayer, 'animation_finished')
 	queue_free()
 
 func _process(delta):
 	if not bullet_ref.get_ref(): return
-	var point = bullet.position
-	add_point(point)
+	points[1] = bullet.position
