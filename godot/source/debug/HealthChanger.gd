dec408df5eb1193b8d282ba07640b730a5b14d64
Dlean Jeans <dleanjeans@gmail.com>
2018-10-24 22:08:02 +0700

    Clean up Debug

--- godot/source/debug/HealthChanger.gd
+++ godot/source/debug/HealthChanger.gd
@@ -4,7 +4,7 @@ export(int) var hp = 5
 export(bool) var kill_allowed = false
 
 func _process(delta):
-	var player = get_parent().player
+	var player = GameData.get_data('player')
 	if Input.is_action_pressed('increase_health'):
 		player.hp = clamp(player.hp + hp, 10, 200)
 	elif Input.is_action_pressed('decrease_health'):
